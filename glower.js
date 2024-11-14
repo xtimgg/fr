4b66c453b383610deddb676a0e04ec479a63c299
xTim <141374121+xtimgg@users.noreply.github.com>
2024-11-14 01:39:35 +0100

    Update glower.js

--- glower.js
+++ glower.js
@@ -12,21 +12,37 @@ spreadSlider.addEventListener("input", () => updateGlow());
 const blendSlider = document.getElementById('glowBlend');
 const blendNum = document.getElementById('glowBlendNum');
 blendSlider.addEventListener("input", () => updateGlow());
+const hueSlider = document.getElementById('glowHue');
+const hueNum = document.getElementById('glowHueColor');
+hueSlider.addEventListener("input", () => updateGlow());
 const codeSnippet = document.getElementById('glowStyling').textContent;
 
 function updateGlow() {
     strengthNum.textContent = strengthSlider.value/100;
     spreadNum.textContent = spreadSlider.value/5;
     blendNum.textContent = blendSlider.value/100;
+    hueNum.style.color = `hsl(${hueSlider.value}, 100%, 50%)`;
     var _spread = spreadSlider.value/5;
+
+    if(document.getElementById('textHueSwitchHandle').style.left == `63%`) {
+        glowyText.style.color = `hsl(${hueSlider.value}, 100%, 80%)`
+    } else {
+        glowyText.style.color = `white`
+    }
+
+    if(document.getElementById('glowHueSwitchHandle').style.left == `63%`) {
+        var glowHSL = `hsl(${hueSlider.value}, 100%, 50%`
+    } else {
+        var glowHSL = `hsl(0, 0%, 100%`
+    }
 
     var glowpx = 2 ** (_spread*2);
     _spread += (_spread-1)/2;
 
-    var glow = `0 0 ${twoDigitsRound(glowpx)}px rgba(255, 255, 255, ${twoDigitsRound((strengthSlider.value/100)*(blendSlider.value/100))})`;
+    var glow = `0 0 ${twoDigitsRound(glowpx)}px ${glowHSL}, ${twoDigitsRound((strengthSlider.value/100)*(blendSlider.value/100))})`;
     for(i=0; i <= spreadSlider.value; i++) {
         glowpx *= 2;
-        glow += `, 0 0 ${twoDigitsRound(glowpx)}px rgba(255, 255, 255, ${twoDigitsRound((strengthSlider.value/glowpx)*(blendSlider.value/100))})`;
+        glow += `, 0 0 ${twoDigitsRound(glowpx)}px ${glowHSL}, ${twoDigitsRound((strengthSlider.value/glowpx)*(blendSlider.value/100))})`;
     }
     glowyText.style.textShadow = glow;
 
@@ -45,3 +61,30 @@ document.getElementById('copyCode').addEventListener("click", () => {
     navigator.clipboard.writeText(glowStyling.textContent);
     setTimeout(() => document.getElementById('copyCode').textContent = `copy\ncode`, 1500)
 });
+
+function switching(switchid, handleid, value) {
+    const switchC = document.getElementById(switchid);
+    const switchH = document.getElementById(handleid);
+    switchC.addEventListener('click', () => {
+        switchH.style.animation = "none";
+        void switchH.offsetWidth;
+        switchH.style.animation = "";
+        if(value) {
+            switchH.style.animation = `switchoff .3s cubic-bezier(.28,1.41,.64,1)`;
+            switchC.style.filter = `grayscale(1)`;
+            switchH.style.left = `5%`;
+            value = false;
+        } else {
+            switchH.style.animation = `switch .3s cubic-bezier(.28,1.41,.64,1)`;
+            switchC.style.filter = `grayscale(0)`;
+            switchH.style.left = `63%`;
+            value = true;
+        }
+    });
+}
+
+let glowColor = false;
+let textColor = false;
+
+switching('glowHueSwitch', 'glowHueSwitchHandle', glowColor);
+switching('textHueSwitch', 'textHueSwitchHandle', textColor);
